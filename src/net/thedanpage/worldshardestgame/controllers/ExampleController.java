dc7c7f447a10e03eac1304908dbc7d7f9f27ee87
Frederik Dam Christensen <fdc@shape.dk>
2018-10-12 11:10:12 +0200

    cleanup controller

--- src/net/thedanpage/worldshardestgame/controllers/ExampleController.java
+++ src/net/thedanpage/worldshardestgame/controllers/ExampleController.java
@@ -8,13 +8,8 @@ import java.util.Random;
 
 public class ExampleController extends Controller {
     Random random = new Random();
-    int wtf = 0;
     @Override
     public Move getMove(GameLevel game) {
-        wtf++;
-        if(0 < wtf && wtf < 1000) { return Move.DOWN; }
-        else if (1000 < wtf && wtf < 2000) { return Move.RIGHT; }
-        else return Move.UP;
-        //return Move.values()[random.nextInt(Move.values().length)];
+        return Move.values()[random.nextInt(Move.values().length)];
     }
 }
